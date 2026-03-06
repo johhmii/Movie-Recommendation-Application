237dfd64eda3ecefc2a9472de1a780c2703836a4
Johnny Rivera <johnnyr.97@csu.fullerton.edu>
2026-03-06 09:52:35 -0800

    Added contributors in header and added function to set title of window.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+#Movie Recommendation App
+#Contributors:
+#Johnny Rivera
 #
-
-
-
+#
+#
 #
 
 import tkinter as tk
 
-root = tk.Tk()
 
+#Creates main window
+root = tk.Tk()
+#Sets the title of the window (Need name for app)
+root.title("Movie Recommendation App")
 
 
 
