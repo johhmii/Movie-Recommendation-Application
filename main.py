e8b8db11701a9846e93dcc875784d6cbf48d28a3
Johnny Rivera <johnnyr.97@csu.fullerton.edu>
2026-03-08 21:23:34 -0700

    Place holder geometry and basic search bar implementation.

--- main.py
+++ main.py
@@ -7,12 +7,24 @@
 #Andy Vu
 
 import tkinter as tk
-
+KEY = "8b3549b6ca7e044a906dea4d61a34125"
 
 #Creates main window
 root = tk.Tk()
 #Sets the title of the window (Need name for app)
 root.title("Movie Recommendation App")
+root.geometry("600x400")
+
+# Create search bar
+search_bar = tk.Entry(root, font=("Helvetica", 20), width = 28)
+search_bar.pack(pady=20, padx=20)
+
+searchResults = tk.Listbox(root, font=("Helvetica", 20), height = 20)
+searchResults.pack(fill=tk.BOTH, expand = True, padx = 20, pady = 20)
+
+
+
+root.mainloop()
 
 
 
